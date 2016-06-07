cd974e07a3e71046a537b6d282836016dd61e1cd
skynowa <skynowa@gmail.com>
2016-06-07 10:35:30 +0300

    FindMySQL: review

--- FindMySQL.cmake
+++ FindMySQL.cmake
@@ -10,28 +10,55 @@
 
 
 #--------------------------------------------------------------------------------------------------
-find_path(MYSQL_INCLUDES
-    NAMES
-        "mysql.h"
-    PATHS
-        "/usr/include"
-        "/usr/local/include"
-        "/usr/mysql/include"
-    PATH_SUFFIXES
-        "mysql")
+if (WIN32)
+    find_path(MYSQL_INCLUDES
+        NAMES
+            "mysql.h"
+        PATHS
+            "$ENV{PROGRAMFILES}/MySQL/*/include"
+            "$ENV{PROGRAMFILES(x86)}/MySQL/*/include"
+            "$ENV{SYSTEMDRIVE}/MySQL/*/include"
+        PATH_SUFFIXES
+            "mysql")
 
-find_library(MYSQL_LIBRARIES
-    NAMES
-        mysqlclient mysqlclient_r
-    PATHS
-        "/lib"
-        "/usr/lib"
-        "/usr/local/lib"
-        "/lib64"
-        "/usr/lib64"
-        "/usr/local/lib64"
-    PATH_SUFFIXES
-        "mysql")
+    find_library(MYSQL_LIBRARIES
+        NAMES
+            "mysqlclient"
+            "mysqlclient_r"
+        PATHS
+            "$ENV{PROGRAMFILES}/MySQL/*/lib"
+            "$ENV{PROGRAMFILES(x86)}/MySQL/*/lib"
+            "$ENV{SYSTEMDRIVE}/MySQL/*/lib"
+        PATH_SUFFIXES
+            "mysql")
+
+else()
+    find_path(MYSQL_INCLUDES
+        NAMES
+            "mysql.h"
+        PATHS
+            "/usr/include"
+            "/usr/local/include"
+            "/usr/mysql/include"
+        PATH_SUFFIXES
+            "mysql")
+
+    find_library(MYSQL_LIBRARIES
+        NAMES
+            "mysqlclient"
+            "mysqlclient_r"
+        PATHS
+            "/lib/mysql"
+            "/lib64/mysql"
+            "/usr/lib/mysql"
+            "/usr/lib64/mysql"
+            "/usr/local/lib/mysql"
+            "/usr/local/lib64/mysql"
+            "/usr/mysql/lib/mysql"
+            "/usr/mysql/lib64/mysql"
+        PATH_SUFFIXES
+            "mysql")
+endif()
 
 if (NOT (MYSQL_INCLUDES AND MYSQL_LIBRARIES))
     set(MYSQL_FOUND 0)
