a568606e8696b8ab8a0d7584c58d024750c90ef8
skynowa <skynowa@gmail.com>
2016-06-07 10:15:39 +0300

    FindMySQL: fix

--- FindMySQL.cmake
+++ FindMySQL.cmake
@@ -3,6 +3,7 @@
 # \brief Find the native MySQL includes and library
 #
 # MYSQL_FOUND     - true if MySQL found
+# xHAVE_MYSQL     - true if MySQL found (source used)
 # MYSQL_INCLUDES  - where to find mysql.h, etc
 # MYSQL_LIBRARIES - list of libraries when using MySQL
 #--------------------------------------------------------------------------------------------------
