4c2a77a97bc95f619bb2a3683fbd63a815359efb
skynowa <skynowa@gmail.com>
2017-04-20 09:24:24 +0300

    FindClangTidy: fix version

--- FindClangTidy.cmake
+++ FindClangTidy.cmake
@@ -8,7 +8,7 @@
 
 
 #--------------------------------------------------------------------------------------------------
-find_program(CLANG_TIDY_FILE_PATH "clang-tidy38")
+find_program(CLANG_TIDY_FILE_PATH "clang-tidy")
 
 if (CLANG_TIDY_FILE_PATH STREQUAL "CLANG_TIDY_FILE_PATH-NOTFOUND")
     set(CLANG_TIDY_FOUND 0)
