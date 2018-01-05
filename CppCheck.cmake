90d1d163f1a468661aaf5a9e0870d646cde293fa
Sergey Shevchenko <skynowa@fabrica.net.ua>
2018-01-05 17:11:09 +0200

    CppCheck: fix

--- CppCheck.cmake
+++ CppCheck.cmake
@@ -1,24 +1,28 @@
-# additional target to perform cppcheck run, requires cppcheck
+#--------------------------------------------------------------------------------------------------
+# \file  CppCheck.cmake
+# \brief Function to register a target_dir for cppcheck
+#
 # https://arcanis.me/ru/2015/10/17/cppcheck-and-clang-format
+#
+#--------------------------------------------------------------------------------------------------
 
-# get all project files
-# HACK this workaround is required to avoid qml files checking ^_^
-file(GLOB_RECURSE ALL_SOURCE_FILES *.cpp *.h)
-foreach (SOURCE_FILE ${ALL_SOURCE_FILES})
-    string(FIND ${SOURCE_FILE} ${PROJECT_TRDPARTY_DIR} PROJECT_TRDPARTY_DIR_FOUND)
-    if (NOT ${PROJECT_TRDPARTY_DIR_FOUND} EQUAL -1)
-        list(REMOVE_ITEM ALL_SOURCE_FILES ${SOURCE_FILE})
-    endif ()
-endforeach ()
 
-add_custom_target(
-        CppCheck
-        COMMAND /usr/bin/cppcheck
-        --enable=warning,performance,portability,information,missingInclude
-        --std=c++11
-        --library=qt.cfg
-        --template="[{severity}][{id}] {message} {callstack} \(On {file}:{line}\)"
-        --verbose
-        --quiet
-        ${ALL_SOURCE_FILES}
-)
+function(perform_cppcheck)
+    message(STATUS "ARGN: ${ARGN}")
+
+    add_custom_target(cppcheck
+        COMMAND
+            ${CPP_CHECK_FILE_PATH}
+                --check-config
+                --library=qt.cfg
+                --enable=warning,performance,portability,information,missingInclude
+                --suppress=missingIncludeSystem
+                --template="[{severity}][{id}] {message} {callstack} \(On {file}:{line}\)"
+                --std=c++11
+                --report-progress
+                --verbose
+                --quiet
+                ${ARGN}
+        WORKING_DIRECTORY
+            ${CMAKE_CURRENT_SOURCE_DIR})
+endfunction()
---
+++ FindCppCheck.cmake
@@ -0,0 +1,35 @@
+#--------------------------------------------------------------------------------------------------
+# \file  FindCppCheck.cmake
+# \brief Find cppcheck
+#
+# CPP_CHECK_FOUND     - true if found
+# CPP_CHECK_FILE_PATH - where to find includes
+#--------------------------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------------------
+find_program(CPP_CHECK_FILE_PATH
+    NAMES
+        "cppcheck"
+    PATHS
+        "/bin"
+        "/usr/bin"
+        "/usr/local/bin"
+    DOC
+        "A tool for static C/C++ code analysis")
+
+if (CPP_CHECK_FILE_PATH STREQUAL "CPP_CHECK_FILE_PATH-NOTFOUND")
+    set(CPP_CHECK_FOUND 0)
+    set(CPP_CHECK_FILE_PATH "")
+else()
+    set(CPP_CHECK_FOUND 1)
+endif()
+#--------------------------------------------------------------------------------------------------
+# trace
+if (NOT CPP_CHECK_FOUND AND CPP_CHECK_FIND_REQUIRED)
+    message(FATAL_ERROR "CPP_CHECK_FOUND: ${CPP_CHECK_FOUND}")
+else()
+    # message(STATUS "CPP_CHECK_FOUND: ${CPP_CHECK_FOUND}")
+    # message("     CPP_CHECK_FILE_PATH: ${CPP_CHECK_FILE_PATH}")
+endif()
+#--------------------------------------------------------------------------------------------------
