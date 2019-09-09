e9ebe8ffe00278a21f8b0178f6bb23ea177099f4
Sergey Shevchenko <skynowa@fabrica.net.ua>
2019-09-09 16:23:34 +0300

    CMakeLists.txt - FindGlobalVariables.cmake, add

---
+++ FindGlobalVariables.cmake
@@ -0,0 +1,16 @@
+#--------------------------------------------------------------------------------------------------
+# \file  FindGlobalVariables.cmake
+# \brief Find globaL variables
+#
+# CMAKE_DATA_DIR_DEFAULT - moduls default directory
+#--------------------------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------------------
+set(CMAKE_DATA_DIR_DEFAULT "/usr/share/cmake-${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION}/Modules")
+#--------------------------------------------------------------------------------------------------
+# trace
+message(STATUS "Module GlobalVariables:")
+message(STATUS "CMAKE_DATA_DIR_DEFAULT: ${CMAKE_DATA_DIR_DEFAULT}")
+message("")
+#--------------------------------------------------------------------------------------------------
