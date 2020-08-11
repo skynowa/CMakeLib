d226eb4293f43edbd139e56dc50f97a14c3f5fae
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-08-12 00:20:07 +0300

    FindGlobalVariables.cmake - CMAKE_ROOT, trace

--- FindGlobalVariables.cmake
+++ FindGlobalVariables.cmake
@@ -11,6 +11,7 @@ set(CMAKE_DATA_DIR_DEFAULT "/usr/share/cmake-${CMAKE_MAJOR_VERSION}.${CMAKE_MINO
 #--------------------------------------------------------------------------------------------------
 # trace
 message(STATUS "Module GlobalVariables:")
-message(STATUS "CMAKE_DATA_DIR_DEFAULT: ${CMAKE_DATA_DIR_DEFAULT}")
+message(STATUS "CMAKE_DATA_DIR_DEFAULT : ${CMAKE_DATA_DIR_DEFAULT}")
+message(STATUS "CMAKE_ROOT             : ${CMAKE_ROOT}")
 message("")
 #--------------------------------------------------------------------------------------------------
