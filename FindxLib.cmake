649c6f919feb5a9c6fe9238fd014ffe7bcd4adb1
Sergey Shevchenko <skynowa@fabrica.net.ua>
2020-09-16 12:40:39 +0300

    FindxLib.cmake - libraries, add

--- FindxLib.cmake
+++ FindxLib.cmake
@@ -35,6 +35,31 @@ else()
     set(xHAVE_XLib 1)
 endif()
 #--------------------------------------------------------------------------------------------------
+# libraries
+set(XLIB_LIBRARIES ${XLIB_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT} ${CMAKE_DL_LIBS})
+
+if     (MSVC)
+    set(XLIB_LIBRARIES ${XLIB_LIBRARIES} advapi32 ws2_32 netapi32 psapi uuid ole32 mpr dbghelp)
+elseif (MINGW)
+    set(XLIB_LIBRARIES ${XLIB_LIBRARIES} advapi32 ws2_32 netapi32 psapi uuid ole32 mpr dbghelp)
+elseif (CMAKE_COMPILER_IS_CLANGXX)
+    add_compile_options(-stdlib=libc++ -lc++abi)
+
+    set(XLIB_LIBRARIES ${XLIB_LIBRARIES} pthread rt m z)
+elseif (CMAKE_COMPILER_IS_GNUCXX)
+    set(XLIB_LIBRARIES ${XLIB_LIBRARIES} pthread rt m z)
+else()
+    message(FATAL_ERROR "xLib: unknown compiler")
+endif()
+
+if (OS_ANDROID)
+    unset(XLIB_DEFINITIONS      CACHE)
+    unset(cmXLIB_COMPILER_FLAGS CACHE)
+    unset(XLIB_LIBRARIES        CACHE)
+
+    set(XLIB_LIBRARIES ${XLIB_LIBRARIES} gnustl_static m z log)
+endif()
+#--------------------------------------------------------------------------------------------------
 # trace
 message(STATUS "XLIB_FOUND: ${XLIB_FOUND}")
 message("   XLIB_INCLUDES:  ${XLIB_INCLUDES}")
