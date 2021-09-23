ca5dfe95c80d93f2e1e2f31a10f4ede4aa64d8a4
skynowa <skynowa@gmail.com>
2021-09-23 09:40:36 +0300

    install.sh - review

--- install.sh
+++ install.sh
@@ -7,7 +7,6 @@
 
 # vars
 PROJECT_NAME="CMakeLib"
-DIR_PROJECT='$PWD'
 DIR_BUILD='../${PROJECT_NAME}_build'
 
 # Build
@@ -16,5 +15,4 @@ echo "Build/Install ..."
 mkdir -p $DIR_BUILD
 cd $DIR_BUILD
 
-cmake $DIR_PROJECT
 sudo cmake --build . --target install
