fd8be7c083eebee45b086019d54c98540bb7d5a7
skynowa <skynowa@gmail.com>
2022-01-08 03:07:37 +0200

    CxxFlags.cmake [7+/6-] - impl

--- CxxFlags.cmake
+++ CxxFlags.cmake
@@ -6,33 +6,34 @@
 # Clamg -
 # MINGW -
 # GCC   - http://gcc.gnu.org/onlinedocs/
+#
+# TODO: https://github.com/ericniebler/meta/blob/master/CMakeLists.txt
 #--------------------------------------------------------------------------------------------------
-
 
 #--------------------------------------------------------------------------------------------------
 if     (MSVC)
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W3")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /permissive- /W4")
 elseif (MINGW)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pipe -Wall -Wno-deprecated -Wextra -Wconversion")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wshadow -Wno-unknown-pragmas -Wno-unused-parameter")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unused-variable -Wno-return-type-c-linkage")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror -Wfatal-errors")
         # -pedantic
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ")
 elseif (CMAKE_COMPILER_IS_CLANGXX)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pipe -Wall -Wno-deprecated -Wextra -Wconversion")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wshadow -Wno-unused-parameter -Wno-unused-variable")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unknown-pragmas -Wno-return-type-c-linkage")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror -fcolor-diagnostics -Qunused-arguments")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror -Wfatal-errors -fcolor-diagnostics -Qunused-arguments")
         # -pedantic
 elseif (CMAKE_COMPILER_IS_GNUCXX)
     if (1)
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -w")
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fmax-errors=1")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wfatal-errors -fmax-errors=1")
         # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pipe -Wall -Wno-deprecated -Wextra -Wconversion")
         # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wshadow -Wno-unknown-pragmas -Wno-unused-parameter")
         # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unused-variable -Wno-return-type-c-linkage")
-        # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror")
+        # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Werror -Wfatal-errors")
         # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fstack-protector -Wformat -Wformat-security")
     else()
         include(CMake/CxxFlags_gcc_4.6.4.cmake)
