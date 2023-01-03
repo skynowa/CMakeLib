7a5a8d74bba72abc6b17cf6de015f5c8740d4d30
skynowa <skynowa@gmail.com>
2023-01-03 19:30:56 +0200

    WarningFlags.cmake [1+/1-] - log(s)

--- WarningFlags.cmake
+++ WarningFlags.cmake
@@ -45,7 +45,7 @@ elseif (CMAKE_COMPILER_IS_GNUCXX)
         include(CMake/WarningFlags_gcc_4.6.4.cmake)
     endif()
 else()
-    message("Unknown compiler")
+    message("WarningFlags: Unknown compiler")
 endif()
 
 if (OS_ANDROID)
