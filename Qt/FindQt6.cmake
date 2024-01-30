62660c01eae63f66afb31c3626866d83b15af92c
skynowa <skynowa@gmail.com>
2024-01-30 23:31:26 +0200

    qt6-port: FindQt6.cmake [36+] - impl

---
+++ Qt/FindQt6.cmake
@@ -0,0 +1,36 @@
+#--------------------------------------------------------------------------------------------------
+# \file  FindQt6.cmake
+# \brief FindQt6 environment
+#--------------------------------------------------------------------------------------------------
+
+
+#--------------------------------------------------------------------------------------------------.
+set(_QT6_MODULES
+    Core
+    Gui
+    Widgets
+    Sql
+    Network
+    Xml
+    PrintSupport
+    Concurrent
+    Core5Compat
+    # Multimedia
+)
+
+if (OS_ANDROID)
+    set(_QT6_MODULES ${_QT6_MODULES} Qml Quick)
+endif()
+
+foreach(COMPONENT ${_QT6_MODULES})
+   find_package(Qt6${COMPONENT} QUIET REQUIRED)
+
+   if ("${COMPONENT}" STREQUAL "Widgets")
+       message("Qt6Widgets version: ${Qt6Widgets_VERSION_STRING}")
+   endif()
+
+   include_directories(${Qt6${COMPONENT}_INCLUDE_DIRS})
+   add_definitions(${Qt6${COMPONENT}_DEFINITIONS})
+   link_libraries(${Qt6${COMPONENT}_LIBRARIES})
+endforeach()
+#--------------------------------------------------------------------------------------------------
