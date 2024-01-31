f6571d2f7b8c34c2e5ef3568c65ec434e412a506
skynowa <skynowa@gmail.com>
2024-02-01 00:52:03 +0200

    qt6-port: InstallDirPatform.cmake [1+] - impl

--- Qt/InstallDirPatform.cmake
+++ Qt/InstallDirPatform.cmake
@@ -8,6 +8,7 @@
 
 
 #--------------------------------------------------------------------------------------------------
+# TODO: Qt6 - impl
 set(CL_INSTALL_DIR_PLATFORM
     "${CMAKE_HOST_SYSTEM_NAME}_${CMAKE_HOST_SYSTEM_PROCESSOR}_Qt${Qt5Core_VERSION_STRING}_${CMAKE_BUILD_TYPE}")
 #--------------------------------------------------------------------------------------------------
