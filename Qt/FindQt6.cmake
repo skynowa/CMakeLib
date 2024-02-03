a1da76ffb2e045c3c1d3df6ea74ab633014b5c42
skynowa <skynowa@gmail.com>
2024-02-03 02:05:40 +0200

    qt6-port: FindQt6.cmake [7+/4-] - trace(s)

--- Qt/FindQt6.cmake
+++ Qt/FindQt6.cmake
@@ -25,12 +25,15 @@ endif()
 foreach(COMPONENT ${_QT6_MODULES})
    find_package(Qt6${COMPONENT} QUIET REQUIRED)
 
-   if ("${COMPONENT}" STREQUAL "Widgets")
-       message("Qt6Widgets version: ${Qt6Widgets_VERSION_STRING}")
-   endif()
-
    include_directories(${Qt6${COMPONENT}_INCLUDE_DIRS})
    add_definitions(${Qt6${COMPONENT}_DEFINITIONS})
    link_libraries(${Qt6${COMPONENT}_LIBRARIES})
 endforeach()
+#--------------------------------------------------------------------------------------------------
+# trace
+message(STATUS "Qt6")
+message("   QT_OFFICIAL_VERSION:       ${QT_OFFICIAL_VERSION}")
+message("   Qt6_VERSION:               ${Qt6_VERSION}")
+message("   Qt6Core_VERSION_STRING:    ${Qt6Core_VERSION_STRING}")
+message("   Qt6Widgets_VERSION_STRING: ${Qt6Widgets_VERSION_STRING}")
 #--------------------------------------------------------------------------------------------------
